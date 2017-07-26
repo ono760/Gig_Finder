9af8220b2ea79afa0c3b4fa47685fbcc0da9c0fa
Onofre Echeverria <ono.ucsb@gmail.com>
2017-07-26 12:54:52 -0700

    minor indentation fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,27 @@ var minify = require('gulp-minify');
 var cleanCss = require('gulp-clean-css');
 
 gulp.task('pack-js', function() {
-    return gulp.src(['public/javascripts/app.js', 'public/javascripts/controllers/*.js'])
-        .pipe(concat('bundle.js'))
-        .pipe(minify({
-            ext: {
-                min: '.js'
-            },
-            noSource: true
-        }))
-        .pipe(gulp.dest('public/build/js'))
+  return gulp.src(['public/javascripts/app.js', 'public/javascripts/controllers/*.js'])
+    .pipe(concat('bundle.js'))
+    .pipe(minify({
+      ext: {
+        min: '.js'
+      },
+      noSource: true
+    }))
+    .pipe(gulp.dest('public/build/js'))
 });
 
 gulp.task('pack-css', function() {
-    return gulp.src(['public/stylesheets/style.css', 'public/stylesheets/buttons/css/*.css'])
-        .pipe(concat('stylesheet.css'))
-        .pipe(cleanCss())
-        .pipe(gulp.dest('public/build/css'))
+  return gulp.src(['public/stylesheets/style.css', 'public/stylesheets/buttons/css/*.css'])
+    .pipe(concat('stylesheet.css'))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('public/build/css'))
 });
 
 gulp.task('watch', function() {
-    gulp.watch('public/javascripts/**/*.js', ['pack-js']);
-    gulp.watch('public/stylesheets/**/*.css', ['pack-css']);
+  gulp.watch('public/javascripts/**/*.js', ['pack-js']);
+  gulp.watch('public/stylesheets/**/*.css', ['pack-css']);
 });
 
 gulp.task('default', ['watch']);
